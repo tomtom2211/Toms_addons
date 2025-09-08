f1f3f6357f11814e5f5c696ede146a4e976fb4b8
tomtom2211 <tomtom2211@seznam.cz>
2025-09-08 17:15:00 +0200

    Initial commit

---
+++ src/main/java/io/github/tomtom2211/tomsaddons/Tomsaddons.java
@@ -0,0 +1,10 @@
+package io.github.tomtom2211.tomsaddons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tomsaddons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/tomtom2211/tomsaddons/client/TomsaddonsClient.java
@@ -0,0 +1,10 @@
+package io.github.tomtom2211.tomsaddons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TomsaddonsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
