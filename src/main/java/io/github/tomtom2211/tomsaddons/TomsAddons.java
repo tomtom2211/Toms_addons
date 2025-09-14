180e46a3b2e8ca6f5e9be9d9c2b3027e9da8ee38
tomtom2211 <tomtom2211@seznam.cz>
2025-09-14 16:20:47 +0200

    Separated StarredMobESP logic from Main class to its own class StarredMobESP.java. Renamed the method jokesLogic to init

--- src/main/java/io/github/tomtom2211/tomsaddons/TomsAddons.java
+++ src/main/java/io/github/tomtom2211/tomsaddons/TomsAddons.java
@@ -1,6 +1,7 @@
 package io.github.tomtom2211.tomsaddons;
 
 import io.github.tomtom2211.tomsaddons.features.Jokes;
+import io.github.tomtom2211.tomsaddons.features.StarredMobESP;
 import io.github.tomtom2211.tomsaddons.modconfig.Config;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -8,19 +9,11 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.render.*;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Objects;
 
 @Environment(EnvType.CLIENT) // To make sure everything is client side
 
@@ -34,37 +27,17 @@ public class TomsAddons implements ModInitializer {
 
         // Jokes.java object (for the initialization of jokeKey mechanic)
         Jokes jokes = new Jokes();
+        StarredMobESP starredMobESP = new StarredMobESP();
         // Keybinds
         KeyBinding jokeKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Funny healer jokeKey", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_J, "Tom's Addons"));
 
         //  End client tick event
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-                jokes.jokeLogic(jokeKey);
+                jokes.init(jokeKey);
         });
 
-        WorldRenderEvents.AFTER_ENTITIES.register(context -> {
-            MinecraftClient client = MinecraftClient.getInstance();
-            if (client.player == null || client.world == null) return;
-
-            MatrixStack matrices = context.matrixStack(); // Gives a current game view state (yaw/distance etc.)
-            VertexConsumerProvider consumers = context.consumers(); // Create a consumer to let minecraft know you want to render something
-            Vec3d cameraPos = context.camera().getPos(); // Camera position
-            for (Entity entity : client.world.getEntities()) {
-                if (entity instanceof ArmorStandEntity armorStand && client.player.canSee(armorStand) && armorStand.getName().getString().contains("✯") && armorStand.isCustomNameVisible())
-                {
-                    Box box = armorStand.getBoundingBox()
-                            .expand(0.3,1,0.3)
-                            .offset(-cameraPos.x, -(cameraPos.y+1), -cameraPos.z); // Absolute => Relative coordinates
-                    VertexConsumer buffer = Objects.requireNonNull(consumers).getBuffer(RenderLayer.getLines());// Something, you can actually write into
-                    VertexRendering.drawBox(
-                            Objects.requireNonNull(matrices),
-                            buffer,
-                            box,
-                            1.0f, 0.0f, 0.0f, 1.0f // RGBA = red
-                    );
-                }
-            }
-        });
+        // World render event
+        WorldRenderEvents.AFTER_ENTITIES.register(starredMobESP::init);
 
 
         // For debugging purposes
---
+++ src/main/java/io/github/tomtom2211/tomsaddons/features/StarredMobESP.java
@@ -0,0 +1,41 @@
+package io.github.tomtom2211.tomsaddons.features;
+
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.VertexRendering;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.Objects;
+
+public class StarredMobESP {
+    public void init(WorldRenderContext context){
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player == null || client.world == null) return;
+
+        MatrixStack matrices = context.matrixStack(); // Gives a current game view state (yaw/distance etc.)
+        VertexConsumerProvider consumers = context.consumers(); // Create a consumer to let minecraft know you want to render something
+        Vec3d cameraPos = context.camera().getPos(); // Camera position
+        for (Entity entity : client.world.getEntities()) {
+            if (entity instanceof ArmorStandEntity armorStand && client.player.canSee(armorStand) && armorStand.getName().getString().contains("✯") && armorStand.isCustomNameVisible())
+            {
+                Box box = armorStand.getBoundingBox()
+                        .expand(0.3,1,0.3)
+                        .offset(-cameraPos.x, -(cameraPos.y+1), -cameraPos.z); // Absolute => Relative coordinates
+                VertexConsumer buffer = Objects.requireNonNull(consumers).getBuffer(RenderLayer.getLines());// Something, you can actually write into
+                VertexRendering.drawBox(
+                        Objects.requireNonNull(matrices),
+                        buffer,
+                        box,
+                        1.0f, 0.0f, 0.0f, 1.0f // RGBA = red
+                );
+            }
+        }
+    }
+}
