1da4b3a0729041d83b03c077c4a8d5079534b4b6
tomtom2211 <tomtom2211@seznam.cz>
2025-11-18 18:58:54 +0100

    Changed the color of Immunity Timers to red Revamped the text shown when Immunity items are on cooldown Added a server timer that is aqua and displays how long it takes the server to deal dmg to you again

--- src/main/java/io/github/tomtom2211/tomsaddons/features/ImmunityTimers.java
+++ src/main/java/io/github/tomtom2211/tomsaddons/features/ImmunityTimers.java
@@ -3,51 +3,74 @@ import io.github.tomtom2211.tomsaddons.modconfig.Config;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.joml.Matrix3x2fStack;
 
 
 public class ImmunityTimers{
     public static long phoenixTimer = 0;
     public static long bonzoTimer = 0;
+    public static long serverTimer = 0;
     public static void init(Text msg){
         if(msg.getString().toLowerCase().contains("phoenix pet saved you")){
             phoenixTimer = System.currentTimeMillis() + 60000;
+            serverTimer = System.currentTimeMillis() + 3000;
         }
         else if(msg.getString().toLowerCase().contains("your bonzo's mask")){
             bonzoTimer = System.currentTimeMillis() + 180000;
+            serverTimer = System.currentTimeMillis() + 3000;
         }
     }
     public static void immunityTimersHUD(){
         HudElementRegistry.attachElementAfter(
-                VanillaHudElements.HOTBAR, // layer to attach before
+                VanillaHudElements.HOTBAR, // layer to attach after
                 Identifier.of("tomsaddons", "after_hotbar_1"),
                 (context, tickCounter) -> {
                     MinecraftClient client = MinecraftClient.getInstance();
-                    if (ImmunityTimers.phoenixTimer > System.currentTimeMillis() && Config.config.immunityTimers) {
+                    if (ImmunityTimers.bonzoTimer > System.currentTimeMillis() && Config.config.immunityTimers) {
                         context.drawText(
                                 client.textRenderer,
-                                (ImmunityTimers.phoenixTimer-System.currentTimeMillis())/1000 + "s Phoenix!",
-                                (client.getWindow().getScaledWidth() / 2)+25,
-                                client.getWindow().getScaledHeight() / 2,
-                                0xFF00FF00,
+                                "Bonzo!" + " (" + (ImmunityTimers.bonzoTimer-System.currentTimeMillis())/1000 + "s)",
+                                (client.getWindow().getScaledWidth() / 2)+10,
+                                client.getWindow().getScaledHeight() / 2-10,
+                                0xffff0000,
                                 true
                         );
                     }
                 }
         );
         HudElementRegistry.attachElementAfter(
-                VanillaHudElements.HOTBAR, // layer to attach before
+                VanillaHudElements.HOTBAR, // layer to attach after
                 Identifier.of("tomsaddons", "after_hotbar_2"),
                 (context, tickCounter) -> {
                     MinecraftClient client = MinecraftClient.getInstance();
-                    if (ImmunityTimers.bonzoTimer > System.currentTimeMillis() && Config.config.immunityTimers) {
+                    if (ImmunityTimers.phoenixTimer > System.currentTimeMillis() && Config.config.immunityTimers) {
                         context.drawText(
                                 client.textRenderer,
-                                (ImmunityTimers.bonzoTimer-System.currentTimeMillis())/1000+"s Bonzo!",
-                                (client.getWindow().getScaledWidth() / 2)+25,
-                                client.getWindow().getScaledHeight() / 2-10,
-                                0xFF00FF00,
+                                "Phoenix!" + " (" + (ImmunityTimers.phoenixTimer-System.currentTimeMillis())/1000 + "s)",
+                                (client.getWindow().getScaledWidth() / 2)+10,
+                                client.getWindow().getScaledHeight() / 2,
+                                0xffff0000,
+                                true
+                        );
+                    }
+                }
+        );
+        HudElementRegistry.attachElementAfter(
+                VanillaHudElements.HOTBAR, // layer to attach after
+                Identifier.of("tomsaddons", "after_hotbar_3"),
+                (context, tickCounter) -> {
+                    MinecraftClient client = MinecraftClient.getInstance();
+                    if (ImmunityTimers.serverTimer > System.currentTimeMillis() && Config.config.immunityTimers) {
+                        double timer = (ImmunityTimers.serverTimer-System.currentTimeMillis())/1000.0;
+                        context.drawText(
+                                client.textRenderer,
+                                String.format("%.2f", timer),
+                                (client.getWindow().getScaledWidth() / 2) - 25,
+                                client.getWindow().getScaledHeight() / 2,
+                                0xFF00FFFF,
                                 true
                         );
                     }
